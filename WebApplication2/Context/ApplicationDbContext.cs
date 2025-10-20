5ee45e8a2cb980d0c27c57dde0fcf5961711e54a
Mehdi Heidari <hymehdi84@gmail.com>
2025-10-20 13:35:06 +0200

    first attempt to build IcodeNext

---
+++ WebApplication2/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=ICodeNext;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
